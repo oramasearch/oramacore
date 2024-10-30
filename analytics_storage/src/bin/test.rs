28ef0748ae7d3d90accc9ea93256a5631cc284db
Michele Riva <michele.riva@oramasearch.com>
2024-10-30 10:25:52 +0100

    handles errors

--- analytics_storage/src/bin/test.rs
+++ analytics_storage/src/bin/test.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use analytics_storage::{AnalyticsStorage, AnalyticsStorageConfig, Granularity, OffloadTarget};
+use anyhow::Context;
+
+fn main() {
+    let storage = AnalyticsStorage::try_new(AnalyticsStorageConfig {
+        index_id: "my_index_id".to_string(),
+        granularity: Some(Granularity::Hour),
+        persistence_dir: None,
+        offload_after: Some(100),
+        offload_to: Some(OffloadTarget::Void),
+        buffer_size: Some(100),
+    })
+    .unwrap();
+}
--- analytics_storage/src/lib.rs
+++ analytics_storage/src/lib.rs
@@ -1,11 +1,14 @@
 use anyhow::{anyhow, Context, Result};
 use ms_converter::ms;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub type TimeSeriesData<T> = Vec<(i64, T)>;
+pub type BlockSpan = (i64, i64);
 
 pub enum Granularity {
     Hour,
@@ -20,16 +23,17 @@ pub enum OffloadTarget {
     R2,
 }
 
+#[derive(Default)]
 pub struct AnalyticsStorageConfig {
-    index_id: String,
-    buffer_size: Option<usize>,
-    granularity: Option<Granularity>,
-    persistence_dir: Option<String>,
-    offload_after: Option<usize>,
-    offload_to: Option<OffloadTarget>,
+    pub index_id: String,
+    pub buffer_size: Option<usize>,
+    pub granularity: Option<Granularity>,
+    pub persistence_dir: Option<String>,
+    pub offload_after: Option<usize>,
+    pub offload_to: Option<OffloadTarget>,
 }
 
-struct AnalyticsStorage {
+pub struct AnalyticsStorage {
     index_id: String,
     docs_size: usize,
     buffer_size: usize,
@@ -39,40 +43,40 @@ struct AnalyticsStorage {
     offload_to: OffloadTarget,
 }
 
-enum Version {
+pub enum Version {
     V1_0,
 }
 
-struct VersionV1_0Schema {
-    ID: String,
-    DeploymentID: String,
-    InstanceID: String,
-    Timestamp: i64,
-    RawSearchString: String,
-    RawQuery: String,
-    ResultsCount: usize,
-    Referer: String,
-    POP: String,
-    Country: String,
-    Continent: String,
-    VisitorID: String,
+pub struct VersionV1_0Schema {
+    id: String,
+    deployment_id: String,
+    instance_id: String,
+    timestamp: i64,
+    raw_search_string: String,
+    raw_query: String,
+    results_count: usize,
+    referer: String,
+    pop: String,
+    country: String,
+    continent: String,
+    visitor_id: String,
 }
 
-struct TimeBlockMeta {
+pub struct TimeBlockMeta {
     doc_size: usize,
     version: Version,
     path: String,
     size: u64,
 }
 
-struct TimeBlock<T> {
+pub struct TimeBlock<T> {
     meta: TimeBlockMeta,
     data: TimeSeriesData<T>,
 }
 
 const ORAMA_ANALYTICS_DEFAULT_DIRNAME: &str = ".orama_analytics";
 const DEFAULT_BUFFER_SIZE: usize = 100;
-const DEFAULT_OFFLOAD_AFTER: usize = 100;
+const DEFAULT_OFFLOAD_AFTER: usize = 1440; // 60 days in hours
 
 impl AnalyticsStorage {
     pub fn try_new(config: AnalyticsStorageConfig) -> Result<Self> {
@@ -96,62 +100,23 @@ impl AnalyticsStorage {
                 .with_context(|| format!("Failed to create directory {}", index_dir))?;
         }
 
+        if !storage.current_block_exists()? {
+            storage.create_new_block_file()?;
+        }
+
         Ok(storage)
     }
 
-    fn get_block(&self, timestamp: i64) -> Result<TimeBlockMeta> {
-        let index_dir = self.get_index_dir();
-
-        let block_lifespan: (i64, i64) = match self.granularity {
-            Granularity::Hour => {
-                let hour = ms("1 hour")?;
-                (timestamp - hour, timestamp + hour)
-            }
-            Granularity::Day => {
-                let day = ms("1 day")?;
-                (timestamp - day, timestamp + day)
-            }
-            Granularity::Week => {
-                let week = ms("1 week")?;
-                (timestamp - week, timestamp + week)
-            }
-            Granularity::Month => {
-                let month = ms("30 days")?;
-                (timestamp - month, timestamp + month)
-            }
-        };
-
-        let block_path = fs::read_dir(index_dir.to_string())
-            .with_context(|| format!("Failed to read directory {}", index_dir))?
-            .map(|res| res.map(|e| e.path().display().to_string()))
-            .find(|file| {
-                if let Ok(file_path) = file {
-                    if let Ok(time) = file_path.parse::<i64>() {
-                        return time > block_lifespan.0 && time < block_lifespan.1;
-                    }
-                }
-                false
-            })
-            .unwrap()?
-            .to_string();
-
-        Self::get_block_meta(&block_path)
-    }
-
-    fn get_index_dir(&self) -> String {
-        format!("{}/{}", self.persistence_dir, self.index_id)
-    }
-
-    fn get_block_meta(path: &str) -> Result<TimeBlockMeta> {
-        let file = fs::File::open(path).map_err(|_| anyhow!("Unable to open file {}", path))?;
+    pub fn get_block_meta(path: &str) -> Result<TimeBlockMeta> {
+        let file = fs::File::open(path).with_context(|| anyhow!("Unable to open file {}", path))?;
         let file_size = file.metadata()?.len();
 
         let reader = BufReader::new(file);
         let mut lines = reader.lines();
 
         if let Some(first_line) = lines.next() {
-            let line =
-                first_line.map_err(|_| anyhow!("Unable to read first line of file {}", path))?;
+            let line = first_line
+                .with_context(|| anyhow!("Unable to read first line of file {}", path))?;
             let headers: Vec<&str> = line.split(',').collect();
 
             if headers.len() < 2 {
@@ -161,7 +126,7 @@ impl AnalyticsStorage {
             let version = Self::parse_version(headers[0])?;
             let docs_size = headers[1]
                 .parse::<usize>()
-                .map_err(|_| anyhow!("Invalid doc_size in header of file {}", path))?;
+                .with_context(|| anyhow!("Invalid doc_size in header of file {}", path))?;
 
             return Ok(TimeBlockMeta {
                 version,
@@ -174,10 +139,83 @@ impl AnalyticsStorage {
         Err(anyhow!("Cannot find header for time block file {}", path))
     }
 
+    fn get_block(&self, timestamp: i64) -> Result<TimeBlockMeta> {
+        let block_path = self.get_current_block_path(Some(timestamp))?;
+        Self::get_block_meta(&block_path)
+    }
+
+    fn get_block_span(&self, timestamp: i64) -> Result<BlockSpan> {
+        match self.granularity {
+            Granularity::Hour => {
+                let hour = ms("1 hour")?;
+                Ok((timestamp - hour, timestamp + hour))
+            }
+            Granularity::Day => {
+                let day = ms("1 day")?;
+                Ok((timestamp - day, timestamp + day))
+            }
+            Granularity::Week => {
+                let week = ms("1 week")?;
+                Ok((timestamp - week, timestamp + week))
+            }
+            Granularity::Month => {
+                let month = ms("30 days")?;
+                Ok((timestamp - month, timestamp + month))
+            }
+        }
+    }
+
+    fn current_block_exists(&self) -> Result<bool> {
+        let path = self.get_current_block_path(None)?;
+        Ok(Path::new(&path).exists())
+    }
+
+    fn get_current_block_path(&self, timestamp: Option<i64>) -> Result<String> {
+        let now = timestamp.unwrap_or_else(|| {
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("WTF time went backwards")
+                .as_millis() as i64
+        });
+
+        let lifespan = self.get_block_span(now)?;
+        let index_dir = self.get_index_dir();
+
+        fs::read_dir(&index_dir)
+            .with_context(|| format!("Failed to read directory {}", index_dir))?
+            .filter_map(|res| res.ok())
+            .map(|entry| entry.path().display().to_string())
+            .find(|file_path| {
+                file_path
+                    .parse::<i64>()
+                    .map(|time| time > lifespan.0 && time < lifespan.1)
+                    .unwrap_or(false)
+            })
+            .ok_or_else(|| anyhow::anyhow!("No matching block found in {}", index_dir))?
+            .parse()
+            .context("Failed to parse block path")
+    }
+
     fn parse_version(version: &str) -> Result<Version> {
         match version {
             "1.0" => Ok(Version::V1_0),
             _ => Err(anyhow!("Invalid time block version number: {}", version)),
         }
+    }
+
+    fn create_new_block_file(&self) -> Result<()> {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)?
+            .as_millis()
+            .to_string();
+        let file_path = format!("{}/{}", self.get_index_dir(), now);
+        let file = File::create(file_path);
+        file?.write_all(b"1.0,0")?;
+
+        Ok(())
+    }
+
+    fn get_index_dir(&self) -> String {
+        format!("{}/{}", self.persistence_dir, self.index_id)
     }
 }
