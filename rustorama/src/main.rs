053d8757ef9740b71f07f88c5b7b0ae17a382717
Tommaso Allevi <tomallevi@gmail.com>
2024-10-31 13:09:44 +0100

    Add e2e test in hurl

--- rustorama/src/main.rs
+++ rustorama/src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use anyhow::Context;
+use anyhow::{Context, Result};
 use collection_manager::{CollectionManager, CollectionsConfiguration};
 use config::Config;
 use rocksdb::OptimisticTransactionDB;
@@ -28,6 +28,12 @@ async fn main() -> anyhow::Result<()> {
         .try_deserialize::<RustoramaConfig>()
         .context("Failed to deserialize configuration")?;
 
+    start(config).await?;
+
+    Ok(())
+}
+
+async fn start(config: RustoramaConfig) -> Result<()> {
     let manager = create_manager(config.clone());
     let manager = Arc::new(manager);
     let web_server = WebServer::new(manager);
@@ -42,4 +48,74 @@ fn create_manager(config: RustoramaConfig) -> CollectionManager {
     let storage = Arc::new(Storage::new(db));
 
     CollectionManager::new(CollectionsConfiguration { storage })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::collections::HashMap;
+    use std::time::Duration;
+    use futures::future::Either;
+    use hurl::runner;
+    use hurl::runner::{Value, RunnerOptionsBuilder};
+    use hurl::util::logger::{LoggerOptionsBuilder, Verbosity};
+    use futures::{future, pin_mut};
+    use tempdir::TempDir;
+    use tokio::time::sleep;
+
+    #[tokio::test]
+    async fn test_hurl() {
+        const HOST: &str = "127.0.0.1";
+        const PORT: u16 = 8080;
+
+        async fn run() {
+            let data_dir = TempDir::new("string_index_test").unwrap();
+            let data_dir: String = data_dir.into_path().to_str().unwrap().to_string();
+            let config = RustoramaConfig {
+                data_dir,
+                http: HttpConfig {
+                    host: HOST.parse().unwrap(),
+                    port: PORT,
+                },
+            };
+            start(config).await.unwrap();
+        }
+        async fn run_test() {
+            sleep(Duration::from_secs(5)).await;
+
+            let content = include_str!("../../api-test.hurl");
+
+            let runner_opts = RunnerOptionsBuilder::new()
+                .follow_location(true)
+                .build();
+            let logger_opts = LoggerOptionsBuilder::new()
+                .verbosity(Some(Verbosity::Verbose))
+                .build();
+
+            let variables: HashMap<_, _> = vec![
+                ("base_url".to_string(), Value::String(format!("http://{}:{}", HOST, PORT))),
+            ].into_iter().collect();
+
+            let result = runner::run(
+                content,
+                None,
+                &runner_opts,
+                &variables,
+                &logger_opts
+            );
+            assert!(result.unwrap().success);
+        }
+
+        let future1 = run();
+        let future2 = run_test();
+
+        pin_mut!(future1);
+        pin_mut!(future2);
+
+        match future::select(future1, future2).await {
+            Either::Left((value1, _)) => value1,
+            Either::Right((value2, _)) => value2,
+        };
+    }
 }
