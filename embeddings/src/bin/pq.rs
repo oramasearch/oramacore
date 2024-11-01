f39032f5d4027141e7cb5b9e63d701a525d8ab42
Michele Riva <michele.riva@oramasearch.com>
2024-11-01 15:39:15 +0100

    wip

--- embeddings/src/bin/pq.rs
+++ embeddings/src/bin/pq.rs
@@ -1,44 +1,33 @@
-use embeddings::product_quantization::*;
-use ndarray::Array2;
 use anyhow::Result;
+use embeddings::load_models;
+use embeddings::product_quantization::*;
 
-fn main() -> Result<()> {
-    let models = embeddings::load_models();
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut quantizer = ProductQuantizer::new(
+        DEFAULT_N_SUBVECTORS,
+        DEFAULT_N_CODES,
+        embeddings::OramaModels::GTESmall.dimensions(),
+        DistanceMetric::Euclidean,
+    );
 
-    let embeds = models.embed(
-        embeddings::OramaModels::GTESmall,
-        vec![
-            "COMFORTABLE FIT WITH VENTILATION.The Nike Everyday Lightweight Footie Socks are made with mesh and a stretchy cotton blend for breathability and a snug, soft feel.BenefitsSweat-wicking fabric helps you stay dry and comfortable.Mesh panels help keep your feet cool.Reinforced toes add durability.Product Details71% cotton/26% nylon/2-3% spandex/0-1% metalized fiberMaterial percentages may vary. Check label for actual content.Machine washImportedShown: White/BlackStyle: SX4863-101".to_string(),
-            "AN ESSENTIAL FOR PRACTICE OR GAME DAY.The Nike Performance Shorts feature a fitted design, while the lined gusset and sweat-wicking technology help keep you dry and comfortable during practice or play.BenefitsDri-FIT technology helps keep you dry and comfortable.3.5&quot; inseam lets you move freely.Lined gusset helps prevent irritation.Elastic waistband stretches for the perfect fit.Product DetailsTight fit for a body-hugging feelBody: 80% polyester/20% spandex. Gusset lining: 100% polyester.Machine washImportedShown: Team Scarlet/Team WhiteStyle: 108720-657".to_string(),
-            "CASUAL COMFORT, SPORTY STYLE.Slide into comfort in the lightweight and sporty Nike Benassi JDI Slide. It features the Nike logo on the foot strap, which is lined in super soft fabric. The foam midsole brings that beach feeling to your feet and adds spring to your kicked-back style.Benefits1-piece, synthetic leather strap is lined with super soft, towel-like fabric.The foam midsole doubles as an outsole, adding lightweight cushioning.Flex grooves let you move comfortably.Shown: Black/WhiteStyle: 343880-090".to_string()
-        ]
-    )?;
+    let models = load_models();
 
-    let lembeds = embeds.first().unwrap();
+    let vectors = models.embed(embeddings::OramaModels::GTESmall, vec![
+        "CASUAL COMFORT, SPORTY STYLE.Slide into comfort in the lightweight and sporty Nike Benassi JDI Slide. It features the Nike logo on the foot strap, which is lined in super soft fabric. The foam midsole brings that beach feeling to your feet and adds spring to your kicked-back style.Benefits1-piece, synthetic leather strap is lined with super soft, towel-like fabric.The foam midsole doubles as an outsole, adding lightweight cushioning.Flex grooves let you move comfortably.Shown: Black/WhiteStyle: 343880-090".to_string(),
+        "CLASSIC SUPPORT AND COMFORT.The Nike Air Monarch IV gives you classic style with real leather and plenty of lightweight Nike Air cushioning to keep you moving in comfort.BenefitsLeather and synthetic leather team up for durability and classic comfort.An Air-Sole unit runs the length of your foot for cushioning, comfort and support.Rubber sole is durable and provides traction".to_string(),
+        "STAY TRUE TO YOUR TEAM ALL DAY, EVERY DAY, GAME DAY.\nRep your favorite team and player anytime in the NFL Baltimore Ravens Game Jersey, inspired by what they&apos;re wearing on the field and designed for total comfort.\nTAILORED FIT\nThis jersey features a tailored fit designed for movement.\n\nLIGHT, SOFT FEEL\nScreen-print numbers provide a light and soft feel".to_string(),
+        "STAY TRUE TO YOUR TEAM ALL DAY, EVERY DAY, GAME DAY.\nRep your favorite team and player anytime in the NFL Indianapolis Colts Game Jersey, inspired by what they&apos;re wearing on the field and designed for total comfort.\nTAILORED FIT\nThis jersey features a tailored fit designed for movement.\n\nCLEAN COMFORT\nThe no-tag neck label offers clean comfort.\n\nLIGHT, SOFT FEEL\nScreen-print numbers provide a light and soft feel.\n\nAdditional Details\n\n\nStrategic ventilation for breathability\nWoven jock tag at front lower left\nTPU shield at V-neck\n\n\n\nFabric: 100% recycled polyester\nMachine Wash\nImportedShown: Gym BlueStyle: 468955-442".to_string(),
+        "A GAME-DAY ESSENTIAL.Featuring comfortable, absorbent fabric, the Nike Swoosh Wristbands stretch with you and keep your hands dry, so you can play your best even when the game heat up.Product DetailsWidth: 3&quot;Sold in pairsSwoosh design embroideryFabric: 72% cotton/12% nylon/11% polyester/4% rubber/1% spandexMachine washImportedShown: White/BlackStyle: NNN04-101".to_string(),
+        "MATCH-READY COMFORT FOR YOUR FEET.The Nike Academy Socks are designed to keep you comfortable during play with soft, sweat-wicking fabric with arch support.BenefitsNike Dri-FIT technology moves sweat away from your skin for quicker evaporation, helping you stay dry and comfortable.Reinforced heel and toe add durability in high-wear areas.Snug band wraps around the arch for a supportive feel.Product DetailsLeft/right specific98% nylon/2% spandexMachine washImportedShown: Varsity Royal/WhiteStyle: SX4120-402".to_string()
+    ])?;
 
-    println!("Embedding length: {}", lembeds.len());
+    quantizer.fit(&vectors, 200);
+    let vector_codes = quantizer.encode::<u32>(&vectors);
+    let closed_vec_indexes = quantizer.search::<u32>(&vectors, &vector_codes);
 
-    if lembeds.is_empty() {
-        return Err(anyhow::anyhow!("Generated embeddings are empty"));
-    }
-
-    let embeddings = Array2::from_shape_vec(
-        (lembeds.len(), embeddings::OramaModels::GTESmall.dimensions()),
-        lembeds.clone(),
-    )?;
-
-    let num_subvectors = DEFAULT_SUBVECTORS;
-    let num_centroids = DEFAULT_CENTROIDS;
-
-    let codebooks = train_pq_codebooks(&embeddings, num_subvectors, num_centroids);
-
-    let quantized_embeddings = quantize_embeddings(&embeddings, &codebooks);
-
-    println!("Quantized Embeddings: {:?}", quantized_embeddings);
-    println!("Codebooks:");
-    for (i, codebook) in codebooks.iter().enumerate() {
-        println!("Codebook {}: {:?}", i, codebook);
-    }
+    println!("{:?}", vector_codes);
+    println!("{:?}", closed_vec_indexes);
 
     Ok(())
 }
--- embeddings/src/product_quantization.rs
+++ embeddings/src/product_quantization.rs
@@ -1,89 +1,236 @@
-use linfa::prelude::*;
-use linfa::DatasetBase;
-use linfa_clustering::KMeans;
-use ndarray::{s, Array1, Array2, ArrayView1, ArrayView2, Axis};
+// Ported from https://github.com/shubham0204/pq.rs/blob/main/src/pq.rs
+// Apache 2.0 license at the moment of porting https://github.com/shubham0204/pq.rs/commit/bf00d22c1e6374116ca987ef4b3e6cbdc4b9c374.
 
-pub const DEFAULT_SUBVECTORS: usize = 16;
-pub const DEFAULT_CENTROIDS: usize = 256;
+use num_traits::cast::FromPrimitive;
+use num_traits::sign::Unsigned;
+use num_traits::ToPrimitive;
+use rand::seq::SliceRandom;
+use std::vec;
 
-pub fn train_pq_codebooks(
-    embeddings: &Array2<f32>,
-    num_subvectors: usize,
-    num_centroids: usize,
-) -> Vec<Array2<f32>> {
-    let embedding_dim = embeddings.shape()[1];
-    let subvector_dim = embedding_dim / num_subvectors;
+pub const DEFAULT_N_SUBVECTORS: usize = 32; // usually we want dimensions/32 or dimensions/16
+pub const DEFAULT_N_CODES: usize = 256;
 
-    let mut codebooks = Vec::new();
+pub enum DistanceMetric {
+    Euclidean,
+    Dot,
+}
 
-    for i in 0..num_subvectors {
-        let subvector_data = embeddings.slice(s![.., i * subvector_dim..(i + 1) * subvector_dim]);
-        let codebook = train_codebook(subvector_data, num_centroids);
-        codebooks.push(codebook);
+pub struct ProductQuantizer {
+    n_subvectors: usize,
+    n_codes: usize,
+    src_vec_dims: usize,
+    codewords: Vec<Vec<Vec<f32>>>,
+    distance_metric: fn(&[f32], &[f32]) -> f32,
+}
+
+impl ProductQuantizer {
+    pub fn new(
+        n_subvectors: usize,
+        n_codes: usize,
+        src_vec_dims: usize,
+        metric: DistanceMetric,
+    ) -> Self {
+        assert!(
+            n_subvectors <= src_vec_dims,
+            "`n_subvectors` has to be smaller than or equal to `src_vec_dims`."
+        );
+        match metric {
+            DistanceMetric::Euclidean => ProductQuantizer {
+                n_subvectors,
+                n_codes,
+                src_vec_dims,
+                codewords: Vec::new(),
+                distance_metric: ProductQuantizer::euclid_distance,
+            },
+            DistanceMetric::Dot => ProductQuantizer {
+                n_subvectors,
+                n_codes,
+                src_vec_dims,
+                codewords: Vec::new(),
+                distance_metric: ProductQuantizer::dot,
+            },
+        }
     }
 
-    codebooks
-}
+    pub fn fit(self: &mut ProductQuantizer, src_vectors: &Vec<Vec<f32>>, iterations: usize) {
+        for vec in src_vectors {
+            assert!(
+                vec.len() == self.src_vec_dims,
+                "Each vector in `src_vectors` must have dims equal to `src_vec_dims`"
+            );
+        }
 
-pub fn quantize_embedding(
-    embedding: &Vec<f32>,
-    codebooks: &Vec<Array2<f32>>,
-) -> Vec<usize> {
-    let embedding_dim = embedding.len();
-    let num_subvectors = codebooks.len();
-    let subvector_dim = embedding_dim / num_subvectors;
+        let sub_vec_dims: usize = self.src_vec_dims / self.n_subvectors;
 
-    let embedding_array = ArrayView1::from(embedding);
+        self.codewords = Vec::new();
+        for m in 0..self.n_subvectors {
+            let mut sub_vectors_m: Vec<Vec<f32>> = Vec::new();
+            for vec in src_vectors {
+                sub_vectors_m.push(vec[(m * sub_vec_dims)..((m + 1) * sub_vec_dims)].to_vec());
+            }
 
-    let mut quantized_embedding = Vec::new();
-    for (i, codebook) in codebooks.iter().enumerate() {
-        let subvector = embedding_array.slice(s![i * subvector_dim..(i + 1) * subvector_dim]);
-        let code_idx = quantize_subvector(subvector, codebook);
-        quantized_embedding.push(code_idx);
+            self.codewords
+                .push(self.kmeans(&sub_vectors_m, self.n_codes, iterations));
+        }
     }
-    quantized_embedding
-}
 
-pub fn quantize_embeddings(
-    embeddings: &Array2<f32>,
-    codebooks: &Vec<Array2<f32>>,
-) -> Vec<Vec<usize>> {
-    embeddings
-        .axis_iter(Axis(0))
-        .map(|embedding| {
-            let embedding_vec = embedding.to_vec();
-            quantize_embedding(&embedding_vec, codebooks)
-        })
-        .collect()
-}
+    pub fn encode<T>(self: &ProductQuantizer, vectors: &Vec<Vec<f32>>) -> Vec<Vec<T>>
+    where
+        T: Unsigned + FromPrimitive,
+    {
+        for vec in vectors {
+            assert!(
+                vec.len() == self.src_vec_dims,
+                "Each vector in `vectors` must have dims equal to `src_vec_dims`"
+            );
+        }
+        let sub_vec_dims: usize = self.src_vec_dims / self.n_subvectors;
+        let mut vector_codes: Vec<Vec<T>> = Vec::new();
+        for vec in vectors {
+            let mut subvectors: Vec<Vec<f32>> = Vec::new();
+            for m in 0..self.n_subvectors {
+                subvectors.push(vec[(m * sub_vec_dims)..((m + 1) * sub_vec_dims)].to_vec());
+            }
+            vector_codes.push(self.vector_quantize(&subvectors));
+        }
+        vector_codes
+    }
 
-fn train_codebook(data: ArrayView2<f32>, num_centroids: usize) -> Array2<f32> {
-    let labels = Array1::from_elem(data.nrows(), 0);
-    let dataset = DatasetBase::new(data.to_owned(), labels);
-    let model = KMeans::params(num_centroids)
-        .fit(&dataset)
-        .expect("KMeans training failed");
+    pub fn search<T>(self: &ProductQuantizer, queries: &[Vec<f32>], codes: &[Vec<T>]) -> Vec<usize>
+    where
+        T: Unsigned + FromPrimitive + ToPrimitive,
+    {
+        for vec in queries {
+            assert!(
+                vec.len() == self.src_vec_dims,
+                "Each vector in `queries` must have dims equal to `src_vec_dims`"
+            );
+        }
+        for code in codes {
+            assert!(
+                code.len() == self.n_subvectors,
+                "Each code in `codes` must have dims equal to `n_codes`"
+            );
+        }
+        let sub_vec_dims: usize = self.src_vec_dims / self.n_subvectors;
+        let mut distances: Vec<usize> = Vec::new();
+        for query in queries {
+            let mut min_distance = f32::MAX;
+            let mut min_distance_index = 0;
+            for (n, code) in codes.iter().enumerate() {
+                let mut distance = 0.0;
+                for m in 0..self.n_subvectors {
+                    let query_sub: Vec<f32> =
+                        query[m * sub_vec_dims..((m + 1) * sub_vec_dims)].to_vec();
 
-    model.centroids().to_owned()
-}
+                    distance += (self.distance_metric)(
+                        &query_sub,
+                        &self.codewords[m][code[m].to_usize().unwrap()],
+                    );
+                }
+                if min_distance > distance {
+                    min_distance = distance;
+                    min_distance_index = n;
+                }
+            }
+            distances.push(min_distance_index);
+        }
+        distances
+    }
 
-fn quantize_subvector(subvector: ArrayView1<f32>, codebook: &Array2<f32>) -> usize {
-    codebook
-        .axis_iter(Axis(0))
-        .enumerate()
-        .min_by(|(_, centroid_a), (_, centroid_b)| {
-            let dist_a = subvector
-                .iter()
-                .zip(centroid_a.iter())
-                .map(|(a, b)| (a - b).powi(2))
-                .sum::<f32>();
-            let dist_b = subvector
-                .iter()
-                .zip(centroid_b.iter())
-                .map(|(a, b)| (a - b).powi(2))
-                .sum::<f32>();
-            dist_a.partial_cmp(&dist_b).unwrap()
-        })
-        .map(|(idx, _)| idx)
-        .unwrap()
+    fn vector_quantize<T>(self: &ProductQuantizer, vector: &[Vec<f32>]) -> Vec<T>
+    where
+        T: FromPrimitive + Unsigned,
+    {
+        let mut codes: Vec<T> = Vec::new();
+        for (m, subvector) in vector.iter().enumerate() {
+            let mut min_distance: f32 = f32::MAX;
+            let mut min_distance_code_index: T = T::from_u8(0).unwrap();
+            for (k, code) in self.codewords[m].iter().enumerate() {
+                let distance = (self.distance_metric)(subvector, code);
+                if distance < min_distance {
+                    min_distance = distance;
+                    min_distance_code_index = T::from_usize(k).unwrap();
+                }
+            }
+            codes.push(min_distance_code_index);
+        }
+
+        codes
+    }
+
+    fn kmeans(
+        self: &ProductQuantizer,
+        vecs: &Vec<Vec<f32>>,
+        n_clusters: usize,
+        iter: usize,
+    ) -> Vec<Vec<f32>> {
+        let mut assigned_centroids: Vec<Vec<f32>> = vec![Vec::new(); vecs.len()];
+        let mut centroids: Vec<Vec<f32>> = vecs
+            .choose_multiple(&mut rand::thread_rng(), n_clusters)
+            .cloned()
+            .collect();
+
+        let vec_dims: usize = vecs[0].len();
+
+        for _ in 0..iter {
+            for i in 0..vecs.len() {
+                let mut min_centroid_distance: f32 = f32::MAX;
+                let mut min_centroid: Vec<f32> = centroids[0].clone();
+                for centroid in &centroids {
+                    // Calculate distance between ith vector and `centroid`
+                    let distance: f32 = (self.distance_metric)(&vecs[i], centroid);
+                    if distance < min_centroid_distance {
+                        min_centroid_distance = distance;
+                        min_centroid = centroid.clone();
+                    }
+                }
+                assigned_centroids[i] = min_centroid;
+            }
+
+            for i in 0..n_clusters {
+                let mut vec_sum: Vec<f32> = vec![0.0; vec_dims];
+                let mut count: usize = 0;
+
+                for j in 0..assigned_centroids.len() {
+                    if assigned_centroids[j] == centroids[i] {
+                        ProductQuantizer::vec_add(&mut vec_sum, &vecs[j]);
+                        count += 1;
+                    }
+                }
+                ProductQuantizer::vec_scale(&mut vec_sum, 1.0 / (count as f32));
+                centroids[i] = vec_sum;
+            }
+        }
+
+        centroids
+    }
+
+    fn vec_scale(vec: &mut Vec<f32>, scale: f32) {
+        for element in vec.iter_mut() {
+            *element *= scale;
+        }
+    }
+
+    fn vec_add(vec1: &mut Vec<f32>, vec2: &[f32]) {
+        for i in 0..vec1.len() {
+            vec1[i] += vec2[i];
+        }
+    }
+
+    fn euclid_distance(vec1: &[f32], vec2: &[f32]) -> f32 {
+        let mut squared_diff_sum = 0.0;
+        for i in 0..vec1.len() {
+            squared_diff_sum += (vec1[i] - vec2[i]).powi(2);
+        }
+        squared_diff_sum.sqrt()
+    }
+
+    fn dot(vec1: &[f32], vec2: &[f32]) -> f32 {
+        let mut dot_product = 0.0;
+        for i in 0..vec1.len() {
+            dot_product += vec1[i] * vec2[i];
+        }
+        dot_product
+    }
 }
