9fd16923a0815c7e3de42f6c7968d11f140c0dec
Michele Riva <michele.riva@oramasearch.com>
2024-11-05 10:36:55 +0100

    adds insert and search

--- vector_index/src/lib.rs
+++ vector_index/src/lib.rs
@@ -1,10 +1,13 @@
+use anyhow::Result;
 use embeddings::OramaModels;
+use hora::core::ann_index::ANNIndex;
+use hora::core::metrics::Metric::CosineSimilarity;
 use hora::index::hnsw_idx;
 
 pub struct VectorIndex {
     pub embeddings_model: OramaModels,
     pub dimensions: usize,
-    pub idx: hnsw_idx::HNSWIndex<f32, usize>,
+    pub idx: hnsw_idx::HNSWIndex<f32, String>,
 }
 
 pub struct VectorIndexConfig {
@@ -15,7 +18,7 @@ impl VectorIndex {
     pub fn new(config: VectorIndexConfig) -> Self {
         let dimensions = config.embeddings_model.dimensions();
 
-        let idx = hnsw_idx::HNSWIndex::<f32, usize>::new(
+        let idx = hnsw_idx::HNSWIndex::<f32, String>::new(
             dimensions,
             &hora::index::hnsw_params::HNSWParams::<f32>::default(),
         );
@@ -25,5 +28,22 @@ impl VectorIndex {
             embeddings_model: config.embeddings_model,
             dimensions,
         }
+    }
+
+    pub fn insert(&mut self, id: String, vector: &[f32]) -> Result<(), &str> {
+        self.idx.add(vector, id)?;
+        self.idx.build(CosineSimilarity)
+    }
+
+    pub fn insert_batch(&mut self, data: Vec<(String, &[f32])>) -> Result<(), &str> {
+        for (id, vector) in data.iter() {
+            self.idx.add(vector, id.clone())?
+        }
+
+        self.idx.build(CosineSimilarity)
+    }
+
+    pub fn search(&mut self, target: &[f32], k: usize) -> Vec<String> {
+        self.idx.search(target, k)
     }
 }
