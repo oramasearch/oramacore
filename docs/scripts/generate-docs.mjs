7fde6bb92de5b2454c31e9fa8db78c6252ce94de
Michele Riva <ciao@micheleriva.it>
2025-01-10 10:43:03 +0100

    ref: removes docs dir

--- docs/scripts/generate-docs.mjs
+++
@@ -1,9 +0,0 @@
-import { generateFiles } from 'fumadocs-openapi';
-import { rimrafSync } from 'rimraf';
-
-rimrafSync('./content/docs/rest-api')
-
-void generateFiles({
-    input: ['./orama-api.yaml'],
-    output: './content/docs/rest-api',
-});
