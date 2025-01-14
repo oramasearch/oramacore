a9c053307db18a3cb3513f713cdf8a69deaedc7c
Michele Riva <ciao@micheleriva.it>
2025-01-14 15:34:46 +0100

    wip

--- src/indexes/vector/hnsw/mod.rs
+++ src/indexes/vector/hnsw/mod.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use rand::rngs::ThreadRng;
+use rand::Rng;
 use simsimd::SpatialSimilarity;
 use std::cmp::{Ord, Ordering, Reverse};
 use std::collections::{BinaryHeap, HashMap};
@@ -64,18 +66,145 @@ struct Graph<K: Ord + Hash + Clone> {
     ml: f64,
     ef_search: usize,
     layers: Vec<Layer<K>>,
+    size: usize,
 }
 
 impl<K: Ord + Hash + Clone> Graph<K> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             distance: f32::cosine,
-            rng: rand::SeedableRng::from_entropy(),
+            rng: rand::thread_rng(),
             m: 16,
             ml: 0.25,
             ef_search: 20,
             layers: Vec::new(),
+            size: 0,
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    pub fn add(&mut self, nodes: Vec<Node<K>>) -> Result<()> {
+        for node in nodes {
+            let key = node.key.clone();
+            let vec = node.value.clone();
+            let insert_level = self.random_level()?;
+
+            while insert_level >= self.layers.len() {
+                self.layers.push(Layer::new());
+            }
+
+            if insert_level < 0 {
+                anyhow::bail!("Invalid level")
+            }
+
+            let mut elevator: Option<Box<K>> = None;
+            let pre_len = self.len();
+
+            for i in (0..=self.layers.len() - 1).rev() {
+                let mut layer = &mut self.layers[i];
+                let mut new_node = Box::new(LayerNode {
+                    node: Node {
+                        key: key.clone(),
+                        value: vec.clone(),
+                    },
+                    neighbors: Some(HashMap::new()),
+                });
+
+                if layer.entry().is_none() {
+                    let mut nodes = HashMap::new();
+                    nodes.insert(key.clone(), new_node);
+                    layer.nodes = nodes;
+                    continue;
+                }
+
+                let search_point = if let Some(elevator_key) = &elevator {
+                    layer.nodes.get(&**elevator_key).unwrap()
+                } else {
+                    layer.entry().unwrap()
+                };
+
+                let neighborhood = search_point.search(self.m, self.ef_search, &vec, self.distance);
+
+                if neighborhood.is_empty() {
+                    anyhow::bail!("No nodes found");
+                }
+
+                elevator = Some(Box::new(neighborhood[0].node.node.key.clone()));
+
+                if insert_level >= i {
+                    if layer.nodes.contains_key(&key) {
+                        // perdoname @tommaso for mi vida loca
+                        let _ = layer;
+                        self.delete(&key);
+                        layer = &mut self.layers[i];
+                    }
+
+                    layer.nodes.insert(key.clone(), new_node.clone());
+
+                    for candidate in neighborhood {
+                        let node = layer.nodes.get_mut(&candidate.node.node.key).unwrap();
+                        node.add_neighbor(new_node.clone(), self.m, self.distance);
+                        new_node.add_neighbor(Box::new((**node).clone()), self.m, self.distance);
+                    }
+                }
+            }
+
+            self.size += 1;
+            if self.len() != pre_len + 1 {
+                anyhow::bail!("Node not added")
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn delete(&mut self, key: &K) -> bool {
+        if self.layers.is_empty() {
+            return false;
+        }
+
+        let mut deleted = false;
+
+        for layer in &mut self.layers {
+            if let Some(node) = layer.nodes.get(key) {
+                let mut node_clone = (**node).clone();
+                layer.nodes.remove(key);
+                node_clone.isolate(self.m);
+                deleted = true
+            }
+        }
+
+        deleted
+    }
+
+    fn random_level(&mut self) -> Result<usize> {
+        let mut max = 1;
+        if self.layers.len() > 0 {
+            if self.ml == 0.0 {
+                anyhow::bail!("ml must be greater than 0.0");
+            }
+
+            max = max_level(self.ml, self.layers[0].size())?;
+        }
+
+        let mut level = 0;
+        while level < max {
+            let random: f64 = self.rng.gen();
+            if random > self.ml {
+                return Ok(level);
+            }
+
+            level += 1;
+        }
+
+        Ok(max)
     }
 
     fn dims(&self) -> usize {
@@ -87,6 +216,10 @@ impl<K: Ord + Hash + Clone> Graph<K> {
             .entry()
             .map(|node| node.node.value.len())
             .unwrap_or(0)
+    }
+
+    fn ptr(v: K) -> Box<K> {
+        Box::new(v)
     }
 }
 
@@ -197,6 +330,22 @@ impl<K: Ord + Hash + Clone> LayerNode<K> {
             .collect()
     }
 
+    fn isolate(&mut self, m: usize) {
+        if let Some(ref mut neighbors) = self.neighbors {
+            for (_, neighbor) in neighbors {
+                if let Some(ref mut neighbor_neighbors) = neighbor.neighbors {
+                    let nodes_to_update: Vec<_> = neighbor_neighbors.values_mut().collect();
+                    for node in nodes_to_update {
+                        if let Some(ref mut node_neighbors) = node.neighbors {
+                            node_neighbors.remove(&self.node.key);
+                            node.replenish(m);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     fn add_neighbor(&mut self, new_node: Box<LayerNode<K>>, m: usize, distance_fn: DistanceFn) {
         let neighbors = self
             .neighbors
@@ -260,4 +409,19 @@ impl<K: Ord + Hash + Clone> LayerNode<K> {
             }
         }
     }
+}
+
+pub fn max_level(ml: f64, num_nodes: usize) -> Result<usize> {
+    if ml == 0.0 {
+        anyhow::bail!("ml must be greater than 0");
+    }
+
+    if num_nodes == 0 {
+        return Ok(1);
+    }
+
+    let level = (num_nodes as f64).ln();
+    let level = level / (1.0 / ml).ln();
+
+    Ok((level.round() as usize) + 1)
 }
