9b4bde3449c01446c8e01767ee42cbb5140a7f90
Tommaso Allevi <tomallevi@gmail.com>
2025-01-22 17:54:12 +0100

    Unique config

--- src/bin/oramacore.rs
+++ src/bin/oramacore.rs
@@ -8,7 +8,7 @@ use tracing::{info, instrument};
 
 #[instrument(level = "info")]
 fn load_config() -> Result<OramacoreConfig> {
-    let config_path = std::env::var("CONFIG_PATH").unwrap_or_else(|_| "./config.jsonc".to_string());
+    let config_path = std::env::var("CONFIG_PATH").unwrap_or_else(|_| "./config.yaml".to_string());
 
     let config_path = PathBuf::from(config_path);
     let config_path = fs::canonicalize(&config_path)?;
@@ -17,7 +17,7 @@ fn load_config() -> Result<OramacoreConfig> {
     info!("Reading configuration from {:?}", config_path);
 
     let settings = Config::builder()
-        .add_source(config::File::with_name(&config_path).format(config::FileFormat::Json5))
+        .add_source(config::File::with_name(&config_path).format(config::FileFormat::Yaml))
         .add_source(config::Environment::with_prefix("ORAMACORE"))
         .build()
         .context("Failed to load configuration")?;
