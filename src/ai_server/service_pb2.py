3b88eb4164be6525791e28a4739769d6423e823d
Michele Riva <ciao@micheleriva.it>
2025-01-23 12:01:57 +0100

    feat: adds party planner

--- src/ai_server/service_pb2.py
+++ src/ai_server/service_pb2.py
@@ -17,7 +17,7 @@ _sym_db = _symbol_database.Default()
 
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\rservice.proto\x12\x10orama_ai_service"L\n\x13\x43onversationMessage\x12$\n\x04role\x18\x01 \x01(\x0e\x32\x16.orama_ai_service.Role\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\t"G\n\x0c\x43onversation\x12\x37\n\x08messages\x18\x01 \x03(\x0b\x32%.orama_ai_service.ConversationMessage"}\n\x10\x45mbeddingRequest\x12+\n\x05model\x18\x01 \x01(\x0e\x32\x1c.orama_ai_service.OramaModel\x12\r\n\x05input\x18\x02 \x03(\t\x12-\n\x06intent\x18\x03 \x01(\x0e\x32\x1d.orama_ai_service.OramaIntent"_\n\x11\x45mbeddingResponse\x12\x36\n\x11\x65mbeddings_result\x18\x01 \x03(\x0b\x32\x1b.orama_ai_service.Embedding\x12\x12\n\ndimensions\x18\x02 \x01(\x05"\x1f\n\tEmbedding\x12\x12\n\nembeddings\x18\x01 \x03(\x02"\x9f\x01\n\x0b\x43hatRequest\x12(\n\x05model\x18\x01 \x01(\x0e\x32\x19.orama_ai_service.LLMType\x12\x0e\n\x06prompt\x18\x02 \x01(\t\x12\x34\n\x0c\x63onversation\x18\x03 \x01(\x0b\x32\x1e.orama_ai_service.Conversation\x12\x14\n\x07\x63ontext\x18\x04 \x01(\tH\x00\x88\x01\x01\x42\n\n\x08_context"\x1c\n\x0c\x43hatResponse\x12\x0c\n\x04text\x18\x01 \x01(\t":\n\x12\x43hatStreamResponse\x12\x12\n\ntext_chunk\x18\x01 \x01(\t\x12\x10\n\x08is_final\x18\x02 \x01(\x08"%\n\x12HealthCheckRequest\x12\x0f\n\x07service\x18\x01 \x01(\t"%\n\x13HealthCheckResponse\x12\x0e\n\x06status\x18\x01 \x01(\t*\x7f\n\nOramaModel\x12\x0c\n\x08\x42GESmall\x10\x00\x12\x0b\n\x07\x42GEBase\x10\x01\x12\x0c\n\x08\x42GELarge\x10\x02\x12\x17\n\x13MultilingualE5Small\x10\x03\x12\x16\n\x12MultilingualE5Base\x10\x04\x12\x17\n\x13MultilingualE5Large\x10\x05*%\n\x0bOramaIntent\x12\t\n\x05query\x10\x00\x12\x0b\n\x07passage\x10\x01*U\n\x07LLMType\x12\x15\n\x11\x63ontent_expansion\x10\x00\x12\x1b\n\x17google_query_translator\x10\x01\x12\n\n\x06vision\x10\x02\x12\n\n\x06\x61nswer\x10\x03*+\n\x04Role\x12\x08\n\x04USER\x10\x00\x12\r\n\tASSISTANT\x10\x01\x12\n\n\x06SYSTEM\x10\x02\x32\xdd\x02\n\nLLMService\x12Z\n\x0b\x43heckHealth\x12$.orama_ai_service.HealthCheckRequest\x1a%.orama_ai_service.HealthCheckResponse\x12W\n\x0cGetEmbedding\x12".orama_ai_service.EmbeddingRequest\x1a#.orama_ai_service.EmbeddingResponse\x12\x45\n\x04\x43hat\x12\x1d.orama_ai_service.ChatRequest\x1a\x1e.orama_ai_service.ChatResponse\x12S\n\nChatStream\x12\x1d.orama_ai_service.ChatRequest\x1a$.orama_ai_service.ChatStreamResponse0\x01\x62\x06proto3'
+    b'\n\rservice.proto\x12\x10orama_ai_service"L\n\x13\x43onversationMessage\x12$\n\x04role\x18\x01 \x01(\x0e\x32\x16.orama_ai_service.Role\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\t"G\n\x0c\x43onversation\x12\x37\n\x08messages\x18\x01 \x03(\x0b\x32%.orama_ai_service.ConversationMessage"}\n\x10\x45mbeddingRequest\x12+\n\x05model\x18\x01 \x01(\x0e\x32\x1c.orama_ai_service.OramaModel\x12\r\n\x05input\x18\x02 \x03(\t\x12-\n\x06intent\x18\x03 \x01(\x0e\x32\x1d.orama_ai_service.OramaIntent"_\n\x11\x45mbeddingResponse\x12\x36\n\x11\x65mbeddings_result\x18\x01 \x03(\x0b\x32\x1b.orama_ai_service.Embedding\x12\x12\n\ndimensions\x18\x02 \x01(\x05"\x1f\n\tEmbedding\x12\x12\n\nembeddings\x18\x01 \x03(\x02"%\n\x14PlannedAnswerRequest\x12\r\n\x05input\x18\x01 \x01(\t"%\n\x15PlannedAnswerResponse\x12\x0c\n\x04plan\x18\x01 \x01(\t"\x9f\x01\n\x0b\x43hatRequest\x12(\n\x05model\x18\x01 \x01(\x0e\x32\x19.orama_ai_service.LLMType\x12\x0e\n\x06prompt\x18\x02 \x01(\t\x12\x34\n\x0c\x63onversation\x18\x03 \x01(\x0b\x32\x1e.orama_ai_service.Conversation\x12\x14\n\x07\x63ontext\x18\x04 \x01(\tH\x00\x88\x01\x01\x42\n\n\x08_context"\x1c\n\x0c\x43hatResponse\x12\x0c\n\x04text\x18\x01 \x01(\t":\n\x12\x43hatStreamResponse\x12\x12\n\ntext_chunk\x18\x01 \x01(\t\x12\x10\n\x08is_final\x18\x02 \x01(\x08"%\n\x12HealthCheckRequest\x12\x0f\n\x07service\x18\x01 \x01(\t"%\n\x13HealthCheckResponse\x12\x0e\n\x06status\x18\x01 \x01(\t*\x7f\n\nOramaModel\x12\x0c\n\x08\x42GESmall\x10\x00\x12\x0b\n\x07\x42GEBase\x10\x01\x12\x0c\n\x08\x42GELarge\x10\x02\x12\x17\n\x13MultilingualE5Small\x10\x03\x12\x16\n\x12MultilingualE5Base\x10\x04\x12\x17\n\x13MultilingualE5Large\x10\x05*%\n\x0bOramaIntent\x12\t\n\x05query\x10\x00\x12\x0b\n\x07passage\x10\x01*U\n\x07LLMType\x12\x15\n\x11\x63ontent_expansion\x10\x00\x12\x1b\n\x17google_query_translator\x10\x01\x12\n\n\x06vision\x10\x02\x12\n\n\x06\x61nswer\x10\x03*+\n\x04Role\x12\x08\n\x04USER\x10\x00\x12\r\n\tASSISTANT\x10\x01\x12\n\n\x06SYSTEM\x10\x02\x32\xbf\x03\n\nLLMService\x12Z\n\x0b\x43heckHealth\x12$.orama_ai_service.HealthCheckRequest\x1a%.orama_ai_service.HealthCheckResponse\x12W\n\x0cGetEmbedding\x12".orama_ai_service.EmbeddingRequest\x1a#.orama_ai_service.EmbeddingResponse\x12\x45\n\x04\x43hat\x12\x1d.orama_ai_service.ChatRequest\x1a\x1e.orama_ai_service.ChatResponse\x12S\n\nChatStream\x12\x1d.orama_ai_service.ChatRequest\x1a$.orama_ai_service.ChatStreamResponse0\x01\x12`\n\rPlannedAnswer\x12&.orama_ai_service.PlannedAnswerRequest\x1a\'.orama_ai_service.PlannedAnswerResponseb\x06proto3'
 )
 
 _globals = globals()
@@ -25,14 +25,14 @@ _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "service_pb2", _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
     DESCRIPTOR._loaded_options = None
-    _globals["_ORAMAMODEL"]._serialized_start = 773
-    _globals["_ORAMAMODEL"]._serialized_end = 900
-    _globals["_ORAMAINTENT"]._serialized_start = 902
-    _globals["_ORAMAINTENT"]._serialized_end = 939
-    _globals["_LLMTYPE"]._serialized_start = 941
-    _globals["_LLMTYPE"]._serialized_end = 1026
-    _globals["_ROLE"]._serialized_start = 1028
-    _globals["_ROLE"]._serialized_end = 1071
+    _globals["_ORAMAMODEL"]._serialized_start = 851
+    _globals["_ORAMAMODEL"]._serialized_end = 978
+    _globals["_ORAMAINTENT"]._serialized_start = 980
+    _globals["_ORAMAINTENT"]._serialized_end = 1017
+    _globals["_LLMTYPE"]._serialized_start = 1019
+    _globals["_LLMTYPE"]._serialized_end = 1104
+    _globals["_ROLE"]._serialized_start = 1106
+    _globals["_ROLE"]._serialized_end = 1149
     _globals["_CONVERSATIONMESSAGE"]._serialized_start = 35
     _globals["_CONVERSATIONMESSAGE"]._serialized_end = 111
     _globals["_CONVERSATION"]._serialized_start = 113
@@ -43,16 +43,20 @@ if not _descriptor._USE_C_DESCRIPTORS:
     _globals["_EMBEDDINGRESPONSE"]._serialized_end = 408
     _globals["_EMBEDDING"]._serialized_start = 410
     _globals["_EMBEDDING"]._serialized_end = 441
-    _globals["_CHATREQUEST"]._serialized_start = 444
-    _globals["_CHATREQUEST"]._serialized_end = 603
-    _globals["_CHATRESPONSE"]._serialized_start = 605
-    _globals["_CHATRESPONSE"]._serialized_end = 633
-    _globals["_CHATSTREAMRESPONSE"]._serialized_start = 635
-    _globals["_CHATSTREAMRESPONSE"]._serialized_end = 693
-    _globals["_HEALTHCHECKREQUEST"]._serialized_start = 695
-    _globals["_HEALTHCHECKREQUEST"]._serialized_end = 732
-    _globals["_HEALTHCHECKRESPONSE"]._serialized_start = 734
-    _globals["_HEALTHCHECKRESPONSE"]._serialized_end = 771
-    _globals["_LLMSERVICE"]._serialized_start = 1074
-    _globals["_LLMSERVICE"]._serialized_end = 1423
+    _globals["_PLANNEDANSWERREQUEST"]._serialized_start = 443
+    _globals["_PLANNEDANSWERREQUEST"]._serialized_end = 480
+    _globals["_PLANNEDANSWERRESPONSE"]._serialized_start = 482
+    _globals["_PLANNEDANSWERRESPONSE"]._serialized_end = 519
+    _globals["_CHATREQUEST"]._serialized_start = 522
+    _globals["_CHATREQUEST"]._serialized_end = 681
+    _globals["_CHATRESPONSE"]._serialized_start = 683
+    _globals["_CHATRESPONSE"]._serialized_end = 711
+    _globals["_CHATSTREAMRESPONSE"]._serialized_start = 713
+    _globals["_CHATSTREAMRESPONSE"]._serialized_end = 771
+    _globals["_HEALTHCHECKREQUEST"]._serialized_start = 773
+    _globals["_HEALTHCHECKREQUEST"]._serialized_end = 810
+    _globals["_HEALTHCHECKRESPONSE"]._serialized_start = 812
+    _globals["_HEALTHCHECKRESPONSE"]._serialized_end = 849
+    _globals["_LLMSERVICE"]._serialized_start = 1152
+    _globals["_LLMSERVICE"]._serialized_end = 1599
 # @@protoc_insertion_point(module_scope)
--- src/ai_server/src/prompts/main.py
+++ src/ai_server/src/prompts/main.py
@@ -11,6 +11,8 @@ TemplateKey = Literal[
     "google_query_translator:user",
     "answer:system",
     "answer:user",
+    "party_planner:system",
+    "party_planner:user",
 ]
 
 PROMPT_TEMPLATES: Dict[TemplateKey, PromptTemplate] = {
@@ -18,22 +20,22 @@ PROMPT_TEMPLATES: Dict[TemplateKey, PromptTemplate] = {
     # Vision eCommerce model
     # ------------------------------
     "vision_ecommerce:system": "You are a product description assistant.",
-    "vision_ecommerce:user": lambda prompt, context: f"Describe the product shown in the image. Include details about its mood, colors, and potential use cases.\n\nImage: {prompt}",
+    "vision_ecommerce:user": lambda prompt, _context: f"Describe the product shown in the image. Include details about its mood, colors, and potential use cases.\n\nImage: {prompt}",
     # ------------------------------
     # Vision generic model
     # ------------------------------
     "vision_generic:system": "You are an image analysis assistant.",
-    "vision_generic:user": lambda prompt, context: f"Provide a detailed analysis of what is shown in this image, including key elements and their relationships.\n\nImage: {prompt}",
+    "vision_generic:user": lambda prompt, _context: f"Provide a detailed analysis of what is shown in this image, including key elements and their relationships.\n\nImage: {prompt}",
     # ------------------------------
     # Vision technical documentation model
     # ------------------------------
     "vision_tech_documentation:system": "You are a technical documentation analyzer.",
-    "vision_tech_documentation:user": lambda prompt, context: f"Analyze this technical documentation image, focusing on its key components and technical details.\n\nImage: {prompt}",
+    "vision_tech_documentation:user": lambda prompt, _context: f"Analyze this technical documentation image, focusing on its key components and technical details.\n\nImage: {prompt}",
     # ------------------------------
     # Vision code model
     # ------------------------------
     "vision_code:system": "You are a code analysis assistant.",
-    "vision_code:user": lambda prompt, context: f"Analyze the provided code block, explaining its functionality, implementation details, and intended purpose.\n\nCode: {prompt}",
+    "vision_code:user": lambda prompt, _context: f"Analyze the provided code block, explaining its functionality, implementation details, and intended purpose.\n\nCode: {prompt}",
     # ------------------------------
     # Google Query Translator model
     # ------------------------------
@@ -43,7 +45,7 @@ PROMPT_TEMPLATES: Dict[TemplateKey, PromptTemplate] = {
         'Your reply must be in the following format: {"query": "<translated_query>"}. As you can see, the translated query must be a JSON object with a single key, \'query\', whose value is the translated query. '
         "Always reply with the most relevant and concise query possible in a valid JSON format, and nothing more."
     ),
-    "google_query_translator:user": lambda query, context: f"### Query\n{query}\n\n### Translated Query\n",
+    "google_query_translator:user": lambda query, _context: f"### Query\n{query}\n\n### Translated Query\n",
     # ------------------------------
     # Answer model
     # ------------------------------
@@ -72,4 +74,39 @@ PROMPT_TEMPLATES: Dict[TemplateKey, PromptTemplate] = {
         """
     ),
     "answer:user": lambda context, question: f"### Context\n{context}\n\n### Question\n{question}\n\n",
+    # ------------------------------
+    # Party planner
+    # ------------------------------
+    "party_planner:system": (
+        """
+        You are an AI action planner. Given a set of allowed actions and user input, output a minimal sequence of actions to achieve the desired outcome.
+
+        ### Input Format
+        actions: Array of allowed action names and their descriptions
+        input: User's desired outcome
+
+        ### Output Format 
+        JSON object with array of ordered steps:
+        {
+          "actions": [
+            {
+              "step": "action_name",
+              "description": "Specific description of how to apply this action"
+            }
+          ]
+        }
+
+        ### Constraints
+        - Only use actions from the provided allowed set
+        - Minimize number of steps
+        - Each step must move toward the goal
+        - Return error object if goal is impossible with given actions
+
+        ### Example
+        Input: "Can you give me an example of how my data has to look when using the standard getExpandedRowModel() function?"
+        Actions: ["OPTIMIZE_QUERY", "PERFORM_ORAMA_SEARCH", "CREATE_CODE", "SUMMARIZE_FINDINGS", "GIVE_REPLY"]
+        Output: {"actions":[{ "step": "OPTIMIZE_QUERY", "description": "Optimize query into a more search-friendly query" }, { "step": "PERFORM_SEARCH", "description": "Use optimized query to perform search in the index" }, { "step": "CREATE_CODE", "description": "Craft code examples about using getExpandedRowModel() function" }, { "step": "SUMMARIZE_FINDINGS", "description": "Summarize the findings from the research and code generation" }]}
+        """
+    ),
+    "party_planner:user": lambda input, actions: f"### Input\n{input}\n\n### Actions\n{actions}",
 }
