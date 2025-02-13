72b31d4204e3f598d1c43f4fbf418d037c13cb4b
Tommaso Allevi <tomallevi@gmail.com>
2025-02-13 14:03:44 +0100

    Fix dockerimage size + Dockerfile-cpu

--- docker.sh
+++ docker.sh
@@ -4,6 +4,6 @@ docker run -it \
   -p 8080:8080 \
   -p 50051:50051 \
   -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
-  -v ./config.jsonc:/app/config.jsonc \
+  -v ./config.yaml:/app/config.yaml \
   --gpus all \
   orama-core
