ab62ee31e50a6812833f7818a7ba08e5c50f5bd5
Michele Riva <ciao@micheleriva.it>
2025-03-20 11:28:21 +0100

    updates dockerfile

--- docker.sh
+++ docker.sh
@@ -1,12 +1,16 @@
 
 set -axe
 
-VERSION=1.0.2
+VERSION=1.0.3
 
 docker build -t oramacore .
 docker tag oramacore oramasearch/oramacore:$VERSION
-docker push
+docker push oramasearch/oramacore:$VERSION
+docker tag oramacore oramasearch/oramacore:latest
+docker push oramasearch/oramacore:latest
 
 cd src/ai_server && docker build -t oramacore-ai-server .
 cd src/ai_server && docker tag oramacore-ai-server oramasearch/oramacore-ai-server:$VERSION
-cd src/ai_server && docker push
+cd src/ai_server && docker push oramasearch/oramacore-ai-server:$VERSION
+cd src/ai_server && docker tag oramacore-ai-server oramasearch/oramacore-ai-server:latest
+cd src/ai_server && docker push oramasearch/oramacore-ai-server:latest
