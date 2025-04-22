36b6d63896edb1a889a1a84ffe07719947541831
Michele Riva <ciao@micheleriva.it>
2025-04-22 14:47:17 +0200

    wip

--- src/ai/tools.rs
+++ src/ai/tools.rs
@@ -211,7 +211,7 @@ impl ToolsRuntime {
                             local.spawn_local(async move {
                                 match JSExecutor::try_new(
                                     JSExecutorConfig {
-                                        allowed_hosts: vec!["api.duckduckgo.com".to_string()],
+                                        allowed_hosts: vec!["en.wikipedia.org".to_string()],
                                         max_startup_time: Duration::from_millis(500), // @todo: make this configurable
                                         max_execution_time: Duration::from_secs(3), // @todo: make this configurable
                                         function_name: function_name.clone(),
