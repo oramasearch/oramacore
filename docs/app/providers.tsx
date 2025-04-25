c10ae2f05508c08065833302cf0cd4a945f3857d
Aileen Villanueva <aileenvl@gmail.com>
2025-04-24 19:21:27 -0600

    skip rendering

--- docs/app/providers.tsx
+++ docs/app/providers.tsx
@@ -4,7 +4,9 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import dynamic from 'next/dynamic';
 import type { ReactNode } from 'react';
 
-const SearchDialog = dynamic(() => import('@/app/components/search'));
+const SearchDialog = dynamic(() => import('@/app/components/search'), {
+  ssr: false
+});
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
