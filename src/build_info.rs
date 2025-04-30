a492aea5d87458991fadd6b6104ebaf1cd7a173e
Tommaso Allevi <tomallevi@gmail.com>
2025-04-30 12:15:23 +0200

    update build info to use github env

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,13 +1,16 @@
 static ORAMA_CORE_VERSION: &str = env!("CARGO_PKG_VERSION");
+static GITHUB_REF_NAME: Option<&'static str> = option_env!("GITHUB_REF_NAME");
+
 static ORAMA_CORE_COMMIT: Option<&str> = option_env!("GIT_COMMIT");
+static GITHUB_SHA: Option<&'static str> = option_env!("GITHUB_SHA");
 
 pub fn get_build_info() -> String {
-    let version = ORAMA_CORE_VERSION;
+    let version = GITHUB_REF_NAME.unwrap_or(ORAMA_CORE_VERSION);
     let commit = ORAMA_CORE_COMMIT.unwrap_or("unknown");
     format!("OramaCore version: {} (commit: {})", version, commit)
 }
 pub fn get_build_version() -> String {
-    let version = ORAMA_CORE_VERSION;
+    let version = GITHUB_SHA.unwrap_or(ORAMA_CORE_VERSION);
     let commit = ORAMA_CORE_COMMIT.unwrap_or("unknown");
     format!("{}@{}", version, commit)
 }
