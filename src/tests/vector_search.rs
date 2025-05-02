5df728133208364a35790d70b8d5ebac9c2ec8a9
Tommaso Allevi <tomallevi@gmail.com>
2025-05-02 11:04:48 +0200

    Fix tests on CI

--- src/tests/vector_search.rs
+++ src/tests/vector_search.rs
@@ -126,6 +126,8 @@ async fn test_vector_search_should_work_after_commit() {
         .await
         .unwrap();
 
+    sleep(std::time::Duration::from_millis(500)).await;
+
     let output1 = collection_client
         .search(
             json!({
@@ -240,6 +242,9 @@ export default {
         )
         .await
         .unwrap();
+
+    // Generage embeddings keeps time
+    sleep(std::time::Duration::from_millis(500)).await;
 
     let output = collection_client
         .search(
