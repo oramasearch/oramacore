b5a5bfbf9a0138e5b0497983a250f29f10093f44
Michele Riva <michele.riva@oramasearch.com>
2025-05-16 22:41:41 -0700

    wip

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,12 +1,46 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
+import type { ReactNode } from "react";
+import type { DocsLayoutProps } from "fumadocs-ui/layouts/docs";
+
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { baseOptions, linkItems } from "@/app/layout.config";
+import { source } from "@/lib/source";
+
+const docsOptions: DocsLayoutProps = {
+	...baseOptions,
+	tree: source.pageTree,
+	// @ts-expect-error
+	links: [linkItems[linkItems.length - 1]],
+	githubUrl: "https://github.com/oramasearch",
+	sidebar: {
+		tabs: {
+			transform(option, node) {
+				const meta = source.getNodeMeta(node);
+				if (!meta || !node.icon) return option;
+
+				const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`;
+
+				return {
+					...option,
+					icon: (
+						<div
+							className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-6.5 md:[&_svg]:size-5"
+							style={
+								{
+									color,
+									border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
+									"--tw-ring-color": `color-mix(in oklab, ${color} 20%, transparent)`,
+								} as object
+							}
+						>
+							{node.icon}
+						</div>
+					),
+				};
+			},
+		},
+	},
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions} githubUrl='https://github.com/oramasearch/orama-core'>
-      {children}
-    </DocsLayout>
-  );
+	return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }
--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -1,13 +1,27 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { AlbumIcon, Heart, LayoutTemplate } from "lucide-react";
 
 function Logo() {
-  return (
-    <div className='flex items-center text-base'>
-      <img src={`/logo/orama-logo.svg`} className='h-6 mr-2' alt='OramaSearch Inc. Logo' />
-      OramaCore
-    </div>
-  )
+	return (
+		<div className="flex items-center text-base">
+			<img
+				src="/logo/orama-logo.svg"
+				className="h-6 mr-2"
+				alt="OramaSearch Inc. Logo"
+			/>
+			Orama
+		</div>
+	);
 }
+
+export const linkItems = [
+	{
+		icon: <AlbumIcon />,
+		text: "Blog",
+		url: "/blog",
+		active: "nested-url",
+	},
+];
 
 /**
  * Shared layout configurations
@@ -17,17 +31,15 @@ function Logo() {
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    transparentMode: "always",
-    title: (
-      <Logo />
-    )
-  },
-  links: [
-    {
-      text: 'Docs',
-      url: '/docs',
-      active: 'nested-url',
-    }
-  ],
+	nav: {
+		transparentMode: "always",
+		title: <Logo />,
+	},
+	links: [
+		{
+			text: "Docs",
+			url: "/docs",
+			active: "nested-url",
+		},
+	],
 };
