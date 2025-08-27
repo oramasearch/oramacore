2a8b830a66d6b76a44c9e5182d4b9f16f195355d
Tommaso Allevi <tomallevi@gmail.com>
2025-08-27 12:43:00 +0200

    Fix clippy rule

--- src/pin_rules/reader.rs
+++ src/pin_rules/reader.rs
@@ -58,7 +58,7 @@ impl PinRulesReader {
         match op {
             PinRuleOperation::Insert(rule) => {
                 self.rule_ids_to_delete.retain(|id| id != &rule.id);
-                self.rules.retain(|rule| rule.id != rule.id);
+                self.rules.retain(|r| r.id != rule.id);
                 self.rules.push(rule);
             }
             PinRuleOperation::Delete(rule_id) => {
