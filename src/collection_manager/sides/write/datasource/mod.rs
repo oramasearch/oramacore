60fc947d182691c2888f9d07ea3688564716ee97
Alberto Moretti <moretti919@gmail.com>
2025-10-20 21:57:55 +0200

    refactor: datasource loop

---
+++ src/collection_manager/sides/write/datasource/mod.rs
@@ -0,0 +1,68 @@
+pub mod s3;
+pub mod storage;
+use tokio::time::MissedTickBehavior;
+use tracing::info;
+
+use super::WriteSide;
+use std::{sync::Arc, time};
+use tracing::error;
+
+pub fn start_datasource_loop(
+    write_side: Arc<WriteSide>,
+    mut stop_receiver: tokio::sync::broadcast::Receiver<()>,
+    stop_done_sender: tokio::sync::mpsc::Sender<()>,
+) {
+    std::thread::spawn(async move || {
+        let period = time::Duration::new(5, 0);
+        let start = tokio::time::Instant::now() + period;
+        let mut interval = tokio::time::interval_at(start, period);
+        interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
+
+        'outer: loop {
+            tokio::select! {
+                _ = stop_receiver.recv() => {
+                    info!("Stopping datasource loop");
+                    break 'outer;
+                }
+                _ = interval.tick() => {}
+            };
+
+            // TODO: looks like rusqlite doesn't support sync trait, so for now I've found this
+            // workaround
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+
+            rt.block_on(async {
+                let s3_datasources = write_side.datasource_storage.get().await;
+
+                for (collection_id, indexes) in s3_datasources.iter() {
+                    for (index_id, datasources) in indexes.iter() {
+                        for datasource in datasources {
+                            match &datasource.datasource {
+                                storage::DatasourceKind::S3(s3_datasource) => {
+                                    if let Err(e) = s3::sync_s3_datasource(
+                                        write_side.clone(),
+                                        *collection_id,
+                                        *index_id,
+                                        datasource.id,
+                                        s3_datasource,
+                                    )
+                                    .await
+                                    {
+                                        error!(error = ?e, "Failed to sync S3 datasource");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        if let Err(e) = stop_done_sender.send(()).await {
+            error!(error = ?e, "Cannot send stop signal to writer side");
+        }
+    });
+}
---
+++ src/collection_manager/sides/write/datasource/storage.rs
@@ -0,0 +1,109 @@
+use crate::lock::OramaAsyncLock;
+use crate::types::{CollectionId, IndexId};
+use std::collections::HashMap;
+use std::sync::Arc;
+
+type IndexDatasources = HashMap<IndexId, Vec<DatasourceEntry>>;
+type CollectionDatasources = HashMap<CollectionId, IndexDatasources>;
+
+#[derive(Clone, Debug)]
+pub struct S3 {
+    pub bucket: String,
+    pub region: String,
+    pub access_key_id: String,
+    pub secret_access_key: String,
+    pub endpoint_url: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+pub enum DatasourceKind {
+    S3(S3),
+}
+
+#[derive(Clone, Debug)]
+pub struct DatasourceEntry {
+    pub id: IndexId,
+    pub datasource: DatasourceKind,
+}
+
+pub struct DatasourceStorage {
+    map: Arc<OramaAsyncLock<CollectionDatasources>>,
+}
+
+impl DatasourceStorage {
+    pub fn new() -> Self {
+        Self {
+            map: Arc::new(OramaAsyncLock::new("datasource_storge", HashMap::new())),
+        }
+    }
+
+    pub async fn insert(
+        &self,
+        collection_id: CollectionId,
+        index_id: IndexId,
+        datasource: DatasourceEntry,
+    ) {
+        let mut m = self.map.write("insert").await;
+        m.entry(collection_id)
+            .or_default()
+            .entry(index_id)
+            .or_default()
+            .push(datasource);
+    }
+
+    pub async fn remove_collection(&self, collection_id: CollectionId) {
+        let mut m = self.map.write("remove_collection").await;
+        m.remove(&collection_id);
+    }
+
+    pub async fn remove_index(&self, collection_id: CollectionId, index_id: IndexId) {
+        let mut m = self.map.write("remove_index").await;
+        if let Some(collection_map) = m.get_mut(&collection_id) {
+            collection_map.remove(&index_id);
+            if collection_map.is_empty() {
+                m.remove(&collection_id);
+            }
+        }
+    }
+
+    pub async fn remove_datasource(
+        &self,
+        collection_id: CollectionId,
+        index_id: IndexId,
+        datasource_id: IndexId,
+    ) {
+        let mut m = self.map.write("remove_datasource").await;
+        if let Some(collection_map) = m.get_mut(&collection_id) {
+            if let Some(entries) = collection_map.get_mut(&index_id) {
+                entries.retain(|e| e.id != datasource_id);
+
+                if entries.is_empty() {
+                    collection_map.remove(&index_id);
+                }
+            }
+            if collection_map.is_empty() {
+                m.remove(&collection_id);
+            }
+        }
+    }
+
+    pub async fn get(&self) -> CollectionDatasources {
+        let m = self.map.read("get_by_type").await;
+        let mut result: CollectionDatasources = HashMap::new();
+
+        for (collection_id, indexes) in m.iter() {
+            for (index_id, datasources) in indexes.iter() {
+                let filtered_datasources: Vec<DatasourceEntry> = datasources.to_vec();
+
+                if !filtered_datasources.is_empty() {
+                    result
+                        .entry(*collection_id)
+                        .or_default()
+                        .insert(*index_id, filtered_datasources);
+                }
+            }
+        }
+
+        result
+    }
+}
